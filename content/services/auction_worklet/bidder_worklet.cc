ddfb77447c37b8239a4c741d6804bc9ffb2f9517
Wojciech Rybak <wojciech.rybak@rtbhouse.com>
2021-08-06 14:05:18 +0200

    take script running time as bid duration

--- content/services/auction_worklet/bidder_worklet.cc
+++ content/services/auction_worklet/bidder_worklet.cc
@@ -282,8 +282,6 @@ void BidderWorklet::V8State::GenerateBid() {
     return;
   }
 
-  base::TimeTicks start = base::TimeTicks::Now();
-
   AuctionV8Helper::FullIsolateScope isolate_scope(v8_helper_.get());
   v8::Isolate* isolate = v8_helper_->isolate();
   // Short lived context, to avoid leaking data at global scope between either
@@ -376,6 +374,7 @@ void BidderWorklet::V8State::GenerateBid() {
 
   v8::Local<v8::Value> generate_bid_result;
   std::vector<std::string> errors_out;
+  base::TimeTicks start = base::TimeTicks::Now();
   if (!v8_helper_
            ->RunScript(context, worklet_script_.Get(isolate), "generateBid",
                        args, errors_out)
@@ -383,6 +382,7 @@ void BidderWorklet::V8State::GenerateBid() {
     PostErrorBidCallbackToUserThread(std::move(errors_out));
     return;
   }
+  base::TimeDelta bid_duration = base::TimeTicks::Now() - start;
 
   if (!generate_bid_result->IsObject()) {
     errors_out.push_back(
@@ -433,7 +433,7 @@ void BidderWorklet::V8State::GenerateBid() {
                          parent_,
                          mojom::BidderWorkletBid::New(
                              std::move(ad_json), bid, std::move(render_url),
-                             base::TimeTicks::Now() - start /* bid_duration */),
+                             bid_duration),
                          std::move(errors_out)));
       return;
     }
