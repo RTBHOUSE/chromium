bc44accf94a1ebc1a4c49d7914cdfdac8555445f
Bartosz Los <bartosz.los@rtbhouse.com>
2021-09-28 22:52:57 +0200

    --no-expose-wasm and --jitless removed from auction_v8_helper

--- content/services/auction_worklet/auction_v8_helper.cc
+++ content/services/auction_worklet/auction_v8_helper.cc
@@ -52,17 +52,6 @@ void InitV8() {
   static const char kOptimizeForSize[] = "--optimize_for_size";
   v8::V8::SetFlagsFromString(kOptimizeForSize, strlen(kOptimizeForSize));
 
-  // Running v8 in jitless mode allows dynamic code to be disabled in the
-  // process, and since each isolate is used only once, this may be best for
-  // performance as well.
-  static const char kJitless[] = "--jitless";
-  v8::V8::SetFlagsFromString(kJitless, strlen(kJitless));
-
-  // WebAssembly isn't encountered during resolution, so reduce the
-  // potential attack surface.
-  static const char kNoExposeWasm[] = "--no-expose-wasm";
-  v8::V8::SetFlagsFromString(kNoExposeWasm, strlen(kNoExposeWasm));
-
   gin::IsolateHolder::Initialize(gin::IsolateHolder::kNonStrictMode,
                                  gin::ArrayBufferAllocator::SharedInstance());
 }
