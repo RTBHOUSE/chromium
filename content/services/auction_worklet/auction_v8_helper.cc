5faccd4be6bc6e11c616ebfef7fdbff1d4ed112e
Bartosz Los <bartosz.los@rtbhouse.com>
2021-10-01 00:55:29 +0200

    bid duration logged

--- content/services/auction_worklet/auction_v8_helper.cc
+++ content/services/auction_worklet/auction_v8_helper.cc
@@ -40,6 +40,9 @@
 #include "v8/include/v8-script.h"
 #include "v8/include/v8-template.h"
 
+#include <string>
+#include <iostream>
+
 namespace auction_worklet {
 
 namespace {
@@ -414,6 +417,7 @@ v8::MaybeLocal<v8::Value> AuctionV8Helper::RunScript(
   // Run script.
   v8::TryCatch try_catch(isolate());
   ScriptTimeoutHelper timeout_helper(isolate(), script_timeout_);
+  base::TimeTicks start = base::TimeTicks::Now();
   auto result = local_script->Run(context);
 
   if (try_catch.HasTerminated()) {
@@ -445,6 +449,9 @@ v8::MaybeLocal<v8::Value> AuctionV8Helper::RunScript(
 
   v8::MaybeLocal<v8::Value> func_result = v8::Function::Cast(*function)->Call(
       context, context->Global(), args.size(), args.data());
+
+  base::TimeDelta bid_duration = base::TimeTicks::Now() - start;
+
   if (try_catch.HasTerminated()) {
     error_out.push_back(base::StrCat(
         {script_name, " execution of `", function_name, "` timed out."}));
@@ -454,6 +461,12 @@ v8::MaybeLocal<v8::Value> AuctionV8Helper::RunScript(
     error_out.push_back(FormatExceptionMessage(context, try_catch.Message()));
     return v8::MaybeLocal<v8::Value>();
   }
+
+  std::cerr << "[rtb-chromium-debug]" << script_name << " duration = " << bid_duration.InMicrosecondsF() << std::endl;
+  if (script_name == "generateBid") {
+    error_out.push_back(std::to_string(bid_duration.InMicrosecondsF()));
+  }
+
   return func_result;
 }
 
