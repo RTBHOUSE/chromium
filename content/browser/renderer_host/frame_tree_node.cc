6204853f6dda5216c82851752e7c7994087e00d6
Xiaochen Zhou <xiaochenzh@chromium.org>
2023-05-25 14:43:53 +0000

    Fenced frames: Event reporting support for ad component urn iframes.
    Modify GetFencedFrameProperties to take in a boolean argument, which
    allows clients to obtain the fenced frame properties associated with
    the frame itself. This is useful when the frame invokes event reporting
    APIs as the reporting metadata should be retrieved from the frame
    itself. Without this change, ad component urn iframes nested in the
    main ad fenced frame will use the fenced frame's FencedFrameProperties
    for event reporting, which is incorrect.
    
    This CL also parameterized the test case so that it tests the
    following cases:
    1. Ad in fenced frame, ad component in a nested fenced frame.
    2. Ad in fenced frame, ad component in a nested iframe.
    3. Ad in iframe, ad component in a nested fenced frame.
    4. Ad in iframe, ad component in a nested iframe.
    
    Bug: 1396067
    Change-Id: I56fba3a93f9ce9c4ffbdac5074e3ef701ac25967

--- content/browser/renderer_host/frame_tree_node.cc
+++ content/browser/renderer_host/frame_tree_node.cc
@@ -895,27 +895,24 @@ bool FrameTreeNode::IsInFencedFrameTree() const {
 }
 
 const absl::optional<FencedFrameProperties>&
-FrameTreeNode::GetFencedFrameProperties() {
-  return GetFencedFramePropertiesForEditing();
+FrameTreeNode::GetFencedFrameProperties(bool force_tree_traversal) {
+  return GetFencedFramePropertiesForEditing(force_tree_traversal);
 }
 
 absl::optional<FencedFrameProperties>&
-FrameTreeNode::GetFencedFramePropertiesForEditing() {
-  if (IsInFencedFrameTree()) {
+FrameTreeNode::GetFencedFramePropertiesForEditing(bool force_tree_traversal) {
+  if (!force_tree_traversal && IsInFencedFrameTree()) {
     return frame_tree().root()->fenced_frame_properties_;
   }
 
   // If we might be in a urn iframe, try to find the "urn iframe root",
   // and, if it exists, return the attached `FencedFrameProperties`.
-  if (blink::features::IsAllowURNsInIframeEnabled()) {
-    FrameTreeNode* node = this;
-    while (node->parent()) {
-      CHECK(node->parent()->frame_tree_node());
-      if (node->fenced_frame_properties_.has_value()) {
-        return node->fenced_frame_properties_;
-      }
-      node = node->parent()->frame_tree_node();
+  FrameTreeNode* node = this;
+  while (node) {
+    if (node->fenced_frame_properties_.has_value()) {
+      return node->fenced_frame_properties_;
     }
+    node = node->parent() ? node->parent()->frame_tree_node() : nullptr;
   }
 
   return fenced_frame_properties_;
@@ -927,7 +924,8 @@ void FrameTreeNode::SetFencedFrameAutomaticBeaconReportEventData(
     network::AttributionReportingRuntimeFeatures
         attribution_reporting_runtime_features) {
   absl::optional<FencedFrameProperties>& properties =
-      GetFencedFramePropertiesForEditing();
+      GetFencedFramePropertiesForEditing(
+          /*force_tree_traversal=*/true);
   // `properties` will exist for both fenced frames as well as iframes loaded
   // with a urn:uuid. This allows URN iframes to call this function without
   // getting bad-messaged.
